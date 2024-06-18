8681f41f85878a8abc2c468c01ce40418e0768c1
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-06-18 10:27:45 +0200

    added first part of BaseCommand, added types info in package.json

--- index.js
+++ index.js
@@ -1,60 +1,86 @@
-import { resolve, dirname, basename } from 'node:path';
+const { resolve, dirname, basename } = require('node:path');
+
+function getCallerFilePath() {
+  /* eslint-disable-next-line unicorn/error-message */
+  const stack = new Error().stack.split('\n');
+
+  return stack[2].slice(
+    stack[2].lastIndexOf('(') + 1,
+    stack[2].lastIndexOf('.js') + 3
+  );
+}
 
 /**
  * @typedef {import('.').BaseCommand}BaseCommand
- * @typedef {import('.').CommandOptions}CommandOptions
- */
+ * @typedef {import('.').CommandOptions}CommandOptions*/
 
+/** @type {BaseCommand} */
 class BaseCommand {
-  /**
-   * @param {object}config
-   * @param {Lowercase<string>|undefined}config.name
-   * @param {string|undefined}config.description
-   * @param {{ slash?: Lowercase<string>, prefix?: Lowercase<string> }|undefined}config.aliases
-   * @param {{ usage?: string, examples?: string }|undefined}config.usage
-   * @param {{
-   * client?: (keyof import('discord.js').PermissionFlags)[],
-   * user?: (keyof import('discord.js').PermissionFlags)[]
-   * }|undefined}config.permissions
-   * @param {{ guild?: number, channel?: number, user?: number }|undefined}config.cooldowns
-   * @param {boolean|undefined}config.dmPermission
-   * @param {boolean|undefined}config.disabled
-   * @param {string|undefined}config.disabledReason
-   * @param {CommandOptions[]|undefined}config.options
-   * @param {boolean|undefined}config.beta
-   * @param {string}config.filePath
-   * @param {(this: import('discord.js').ChatInputCommandInteraction | import('discord.js').Message, lang: import('.').lang, client: import('discord.js').Client<true>) => Promise<never>}config.run*/
+  // Thanks to this I have types in the constructor
+  name; category; description; descriptionLocalizations; type; filePath;
+  cooldowns; permissions; defaultMemberPermissions;
+  prefixCommand; slashCommand; dmPermission; disabled;
+  disabledReason; options; beta; run;
+
+  /** @param {import('.').BaseCommandInitOptions}options*/
   constructor({
     name, description,
     aliases = {}, usage = {},
-    permissions: { client = [], slash = [] } = { client: [], slash: [] },
-    cooldowns: { guild = 0, channel = 0, user = 0 } = { guild: 0, channel: 0, user: 0 },
+    permissions = { }, cooldowns = { },
     dmPermission = false,
-    disabled = false,
-    disabledReason,
+    disabled = false, disabledReason,
     options = [],
     beta = false,
-    filePath,
+    filePath = getCallerFilePath(),
     run
   } = {}) {
-    const path = resolve(filePath);
-
-    this.name = (name ?? basename(path)).toLowerCase();
+    this.filePath = resolve(filePath);
+    this.name = (name ?? basename(this.filePath)).toLowerCase();
+    this.nameLocalizations = undefined;
+    this.category = basename(dirname(this.filePath));
     this.description = description;
-    this.category = basename(dirname(path));
+    this.descriptionLocalizations = undefined;
     this.aliases = aliases;
     this.usage = usage;
-    this.permissions = { client, slash };
-    this.cooldowns = { guild, channel, user };
+    this.permissions = permissions;
+    this.cooldowns = {
+      guild: Math.max(cooldowns.guild ?? 0, 0),
+      channel: Math.max(cooldowns.channel ?? 0, 0),
+      user: Math.max(cooldowns.user ?? 0, 0)
+    };
+    this.slashCommand = undefined;
+    this.prefixCommand = undefined;
     this.dmPermission = dmPermission;
     this.disabled = disabled;
     this.disabledReason = disabledReason;
     this.options = options;
     this.beta = beta;
-    this.filePath = path;
-
     this.run = run;
+
+    this.#validateData();
+    this.localize();
+  }
+
+  #validateData() {
+
   }
 }
 
-export default { BaseCommand };
+class SlashCommand extends BaseCommand {
+
+}
+
+class PrefixCommand extends BaseCommand {
+  constructor() {
+    super();
+
+    this.slashCommand = true;
+    this.prefixCommand = false;
+  }
+}
+
+class CommandOptions {
+
+}
+
+module.exports = { BaseCommand, SlashCommand, PrefixCommand, CommandOptions };
