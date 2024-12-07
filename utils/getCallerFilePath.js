fb0bcd9b59b92ce912681c4342c6e001c253ca9c
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-12-07 21:58:35 +0100

    not always re-declaring the same constant

--- utils/getCallerFilePath.js
+++ utils/getCallerFilePath.js
@@ -1,8 +1,7 @@
-module.exports = function getCallerFilePath(dirname) {
-  const
-    /* eslint-disable-next-line unicorn/error-message -- not an Error that will ever get thrown.*/
-    line = new Error().stack.split('\n').find(e => e.includes(dirname)),
-    extention = '.js';
+const fileExtention = '.js';
 
-  return line.slice(line.lastIndexOf('(') + 1, line.lastIndexOf(extention) + extention.length);
+module.exports = function getCallerFilePath(dirname) {
+  /* eslint-disable-next-line unicorn/error-message -- not an Error that will ever get thrown.*/
+  const line = new Error().stack.split('\n').find(e => e.includes(dirname));
+  return line.slice(line.lastIndexOf('(') + 1, line.lastIndexOf(fileExtention) + fileExtention.length);
 };
